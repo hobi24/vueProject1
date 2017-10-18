9d343f11e6f9a6898b7e0aff8fb70e568ca505e2
hobi24 <838411134@qq.com>
2017-10-18 16:36:50 +0800

    添加vue项目

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <div class="header">I am header;</div> -->
+    <v-header></v-header>
+    <div class="tab">
+      <div class="item">商品</div>
+      <div class="item">评价</div>
+      <div class="item">商家</div>
+    </div>
+    <div class="content">I am content</div>
+  </div>
+</template>
+
+<script>
+
+import header from './components/header/header.vue';
+export default {
+  components: {
+    'v-header': header
+  }
+  // name: 'app'
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+      顶部
+  </div>
+</template>
+
+<script>
+export default {
+    // name: 'header'
+};
+</script>
+<style scoped>
+    .header {
+        color: pink;
+    }
+</style>
+
